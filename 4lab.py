31b3642ce0e2d8c6b5df7768cfabc2241a912952
Arsalan <batyavzdanii749@gmail.com>
2024-12-13 20:08:25 +0300

    labi

---
+++ 4lab.py
@@ -0,0 +1,1 @@
+import create_matrix_from_vectors
---
+++ connect_moduls/4laba/1.py
@@ -0,0 +1,6 @@
+import numpy as np
+def create_matrix_from_vectors(vectors):
+    """
+    1. Создать двухмерный массив (матрицу) из нескольких полученных векторов.
+    """
+    return np.array(vectors)
